059986ddd396fd2cce57ae8ed4424b2259a5492e
shivamsoods <shivam.sood708@gmail.com>
2019-07-05 23:12:38 +0530

    Added MCQ

--- app/src/main/java/com/arihantas/eduapp/TestFragment.java
+++ app/src/main/java/com/arihantas/eduapp/TestFragment.java
@@ -2,10 +2,6 @@ package com.arihantas.eduapp;
 
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +11,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,6 +24,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 
 /**
@@ -31,8 +35,7 @@ import java.util.ArrayList;
 public class TestFragment extends Fragment {
 
 
-
-    private Button btnA, btnB, btnC, btnD, btnSubmit;
+    private Button btnA, btnB, btnC, btnD, btnSubmit, btnNext, btnPrev;
     private TextView tvQuestion, tvScore;
     private String optionSelected = "none";
     private int totalItem;
@@ -42,6 +45,8 @@ public class TestFragment extends Fragment {
     private boolean isLoading = true;
     private RelativeLayout rlMcqProgress;
     private FirebaseAuth mAuth;
+    private  boolean allAnswered=false;
+
 
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private DatabaseReference myRef = database.getReference("Question");
@@ -55,13 +60,15 @@ public class TestFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view=inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
 
         btnA = view.findViewById(R.id.btn_mcq_A);
         btnB = view.findViewById(R.id.btn_mcq_B);
         btnC = view.findViewById(R.id.btn_mcq_C);
         btnD = view.findViewById(R.id.btn_mcq_D);
         btnSubmit = view.findViewById(R.id.btn_mcq_submit);
+        btnNext = view.findViewById(R.id.btn_mcq_next);
+        btnPrev = view.findViewById(R.id.btn_mcq_prev);
 
         tvQuestion = view.findViewById(R.id.tv_mcq_question);
         tvScore = view.findViewById(R.id.tv_mcq_score);
@@ -77,7 +84,7 @@ public class TestFragment extends Fragment {
         btnA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                optionSelected = "optA";
+                quesList.get(listItem).setSelAnswer("optA");
                 setButtonColor();
                 btnA.setBackgroundResource(R.color.sky_blue);
 
@@ -87,7 +94,7 @@ public class TestFragment extends Fragment {
         btnB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                optionSelected = "optB";
+                quesList.get(listItem).setSelAnswer("optB");
                 setButtonColor();
                 btnB.setBackgroundResource(R.color.sky_blue);
             }
@@ -95,8 +102,7 @@ public class TestFragment extends Fragment {
         btnC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                optionSelected = "optC";
+                quesList.get(listItem).setSelAnswer("optC");
                 setButtonColor();
                 btnC.setBackgroundResource(R.color.sky_blue);
             }
@@ -104,50 +110,96 @@ public class TestFragment extends Fragment {
         btnD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                optionSelected = "optD";
+                quesList.get(listItem).setSelAnswer("optD");
                 setButtonColor();
                 btnD.setBackgroundResource(R.color.sky_blue);
             }
         });
 
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setButtonColor();
 
-
-                if (listItem < totalItem) {
-                    if (optionSelected.equals(quesList.get(listItem).getAnswer())) {
-                        Toast.makeText(getActivity(), "Correct Answer", Toast.LENGTH_SHORT).show();
-
-                        displayNextQues();
-                        optionSelected = "none";
-
-                        score++;
-                        tvScore.setText("Score: " + score);
-
-                    } else if (optionSelected.equals("none")) {
-                        Toast.makeText(getActivity(), "Please enter a choise", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(getActivity(), "Wrong Answer", Toast.LENGTH_SHORT).show();
-
-                        displayNextQues();
-                        optionSelected = "none";
-
-
-                    }
-                } else {
-                    Toast.makeText(getActivity(), "You have ended the quiz", Toast.LENGTH_SHORT).show();
-                }
-
+                displayNextQues();
 
             }
         });
 
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayPrevQues();
+            }
+        });
+
+//        btnSubmit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                setButtonColor();
+//
+//
+//                if (listItem < totalItem) {
+//                    if (optionSelected.equals(quesList.get(listItem).getAnswer())) {
+//                        Toast.makeText(getActivity(), "Correct Answer", Toast.LENGTH_SHORT).show();
+//
+//                        displayNextQues();
+//                        optionSelected = "none";
+//
+//                        score++;
+//                        tvScore.setText("Score: " + score);
+//
+//                    } else if (optionSelected.equals("none")) {
+//                        Toast.makeText(getActivity(), "Please enter a choise", Toast.LENGTH_SHORT).show();
+//                    } else {
+//                        Toast.makeText(getActivity(), "Wrong Answer", Toast.LENGTH_SHORT).show();
+//
+//                        displayNextQues();
+//                        optionSelected = "none";
+//
+//
+//                    }
+//                } else {
+//                    Toast.makeText(getActivity(), "You have ended the quiz", Toast.LENGTH_SHORT).show();
+//                }
+//
+//
+//            }
+//        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for(int i=0;i<totalItem;i++){
+                    if(quesList.get(i).getSelAnswer().equals(quesList.get(i).getAnswer())){
+                        score++;
+                    }
+                    if(quesList.get(i).getSelAnswer().equals("none")){
+                        allAnswered=false;
+
+                    }
+                    else {
+                        allAnswered=true;
+                    }
+                }
+
+
+                if(allAnswered){
+                    Toast.makeText(getActivity(), "Your Score is "+ score, Toast.LENGTH_LONG).show();
+
+                    assert getFragmentManager() != null;
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_container, new StudyFragment())
+                            .commit();
+                }
+                else {
+                    Toast.makeText(getActivity(), "Answer all the questions", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
         return view;
     }
-
 
 
     private void setDisplay(int item) {
@@ -176,8 +228,8 @@ public class TestFragment extends Fragment {
                     String optD = (String) messageSnapshot.child("optD").getValue();
                     String answer = (String) messageSnapshot.child("answer").getValue();
 
-                    quesList.add(new mcqQuestion(qtext, optA, optB, optC, optD, answer));
-                    Log.d("TOTAL ITEM", String.valueOf(String.valueOf(quesList.size())));
+                    quesList.add(new mcqQuestion(qtext, optA, optB, optC, optD, answer,"none"));
+                    Log.d("TOTAL ITEM", String.valueOf(quesList.size()));
 
                 }
 
@@ -209,11 +261,76 @@ public class TestFragment extends Fragment {
 
     }
 
-    private void displayNextQues() {
-        listItem++;
 
-        if (listItem != totalItem)
+
+    private void displayNextQues() {
+
+        if (listItem < totalItem-1) {
+            listItem++;
             setDisplay(listItem);
+            setButtonColor();
+
+            switch (quesList.get(listItem).getSelAnswer()) {
+                case "optA":
+                    btnA.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optB":
+                    btnB.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optC":
+                    btnC.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optD":
+                    btnD.setBackgroundResource(R.color.sky_blue);
+                    break;
+                default:
+                    setButtonColor();
+                    break;
+
+
+            }
+
+
+        } else {
+            btnNext.setVisibility(View.GONE);
+            btnSubmit.setVisibility(View.VISIBLE);
+        }
+
+
+    }
+
+    private void displayPrevQues() {
+btnNext.setVisibility(View.VISIBLE);
+
+        if (listItem > 0 && listItem < totalItem) {
+            listItem--;
+            setDisplay(listItem);
+            setButtonColor();
+
+            switch (quesList.get(listItem).getSelAnswer()) {
+                case "optA":
+                    btnA.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optB":
+                    btnB.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optC":
+                    btnC.setBackgroundResource(R.color.sky_blue);
+                    break;
+                case "optD":
+                    btnD.setBackgroundResource(R.color.sky_blue);
+                    break;
+                default:
+                    setButtonColor();
+                    break;
+
+
+            }
+
+        } else {
+
+            Toast.makeText(getActivity(), "Cannot GO back!!", Toast.LENGTH_SHORT).show();
+        }
 
 
     }
--- app/src/main/java/com/arihantas/eduapp/mcqQuestion.java
+++ app/src/main/java/com/arihantas/eduapp/mcqQuestion.java
@@ -12,8 +12,10 @@ public class mcqQuestion extends Activity {
     private String optC;
     private String optD;
     private String answer;
+    private String selAnswer;
 
-    public mcqQuestion( String question, String optA, String optB, String optC, String optD, String answer) {
+
+    public mcqQuestion( String question, String optA, String optB, String optC, String optD, String answer,String selAnswer) {
 
         this.question = question;
         this.optA = optA;
@@ -21,6 +23,7 @@ public class mcqQuestion extends Activity {
         this.optC = optC;
         this.optD = optD;
         this.answer = answer;
+        this.selAnswer=selAnswer;
     }
 
 
@@ -72,6 +75,10 @@ public class mcqQuestion extends Activity {
     public void setAnswer(String answer) {
         this.answer = answer;
     }
+
+    public String getSelAnswer() {return selAnswer;}
+
+    public void setSelAnswer(String selAnswer) {this.selAnswer = selAnswer;}
 }
 
 
